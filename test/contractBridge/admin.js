211fd849e3ee0b8c6401e2f0e7a1fc8546b78814
supertree-wayne <wayne@supertree.co>
2024-05-24 16:11:16 +0900

    feat: audit fix - BPD-04 add chain id getter

--- test/contractBridge/admin.js
+++ test/contractBridge/admin.js
@@ -4,10 +4,7 @@
  */
 const TruffleAssert = require('truffle-assertions');
 const Ethers = require('ethers');
-
 const Helpers = require('../helpers');
-const {sendAndAwait} = require("truffle/build/593.bundled");
-
 const BridgeContract = artifacts.require("Bridge");
 const ERC20MintableContract = artifacts.require("ERC20PresetMinterPauser");
 const ERC20HandlerContract = artifacts.require("ERC20Handler");
