d0b2bdeca37e4c92ce93ba4286d80622edd04bec
supertree-wayne <wayne@supertree.co>
2024-05-24 22:51:11 +0900

    feat: audit fix - PDT-06 fix `withdraw()` Can Never Be Called

--- contracts/handlers/ERC20Handler.sol
+++ contracts/handlers/ERC20Handler.sol
@@ -217,13 +217,5 @@ contract ERC20Handler is IDepositExecute, HandlerHelpers, ERC20Safe {
         }
     }
 
-    /**
-        @notice Used to manually release ERC20 tokens from ERC20Safe.
-        @param tokenAddress Address of token contract to release.
-        @param recipient Address to release tokens to.
-        @param amount The amount of ERC20 tokens to release.
-     */
-    function withdraw(address tokenAddress, address recipient, uint amount) external override onlyBridge {
-        releaseERC20(tokenAddress, recipient, amount);
-    }
+
 }
